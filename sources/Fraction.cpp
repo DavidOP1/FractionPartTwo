86fce5831f9f18477078ad9f813fac1b7a393fbb
DavidOP1 <54214707+DavidOP1@users.noreply.github.com>
2023-05-02 17:26:48 +0300

    Update Fraction.cpp

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -91,7 +91,7 @@ namespace ariel{
             cout << "result: " << result << endl;
                         
             int check = static_cast<int>(round(result));
-            if(check>=INT_MAX || check=< INT_MIN){
+            if((check >=INT_MAX) || (check =< INT_MIN)){
                 throw overflow_error("more then int max");
             }
             
